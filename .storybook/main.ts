8c76c9ccfcfd938cb16fcbc97deddb226c93ab45
Quillinan <milton.quillinan@investsmart.com.br>
2026-05-26 03:19:28 -0300

    chore: add tailwind and alias shade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,20 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
+import tailwindcss from "@tailwindcss/vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
-    "@storybook/addon-mcp"
+    "@storybook/addon-mcp",
   ],
-  "framework": "@storybook/react-vite"
+  framework: "@storybook/react-vite",
+
+  viteFinal: async (config) => {
+    config.plugins = [...(config.plugins ?? []), tailwindcss()];
+    return config;
+  },
 };
 export default config;
